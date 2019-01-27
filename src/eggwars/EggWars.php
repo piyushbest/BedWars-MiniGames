0e7305fcc3bd42823b005cecc50a4fe0f3cb2d26
piyushbest <42518831+piyushbest@users.noreply.github.com>
2019-01-27 16:56:11 +0530

    Add files via upload

--- src/eggwars/EggWars.php
+++ src/eggwars/EggWars.php
@@ -1,1 +1,131 @@
-Wait
+<?php
+
+/*
+ *    _____                __        __
+ *   | ____|  __ _    __ _ \ \      / /__ _  _ __  ___
+ *   |  _|   / _` | / _` |  \ \ /\ / // _` || '__|/ __|
+ *   | |___ | (_| || (_| |   \ V  V /| (_| || |   \__ \
+ *   |_____| \__, | \__, |    \_/\_/  \__,_||_|   |___/
+ *           |___/  |___/
+ */
+
+
+declare(strict_types=1);
+
+namespace BedWars;
+
+use eggwars\commands\EggWarsCommand;
+use eggwars\commands\TeamCommand;
+use eggwars\commands\VoteCommand;
+use eggwars\event\listener\ArenaSetupManager;
+use eggwars\event\listener\LevelSetupManager;
+use eggwars\utils\ConfigManager;
+use pocketmine\command\Command;
+use pocketmine\plugin\PluginBase;
+
+/**
+ * Class EggWars
+ * @package eggwars
+ *
+ * @author VixikCZ && ANDREAX22
+ * @version 1.0.0 [DEV]
+ * @api 3.0.0 - 4.0.0
+ *
+ * @copyright (c) ANDREX22 (2017 - 2019)
+ */
+class EggWars extends PluginBase {
+
+    /** @var EggWars $instance */
+    private static $instance;
+
+    /** @var ArenaManager $arenaManager */
+    private $arenaManager;
+
+    /** @var LevelManager $levelManager */
+    private $levelManager;
+
+    /** @var ArenaSetupManager $arenaSetupManager */
+    private $arenaSetupManager;
+
+    /** @var LevelSetupManager $levelSetupManager */
+    private $levelSetupManager;
+
+    /** @var ConfigManager $configManager */
+    private $configManager;
+
+    /** @var Command[] $commands */
+    private $commands = [];
+
+    public function onEnable() {
+        self::$instance = $this;
+        $this->registerCommands();
+        $this->configManager = new ConfigManager;
+        $this->levelManager = new LevelManager;
+        $this->arenaManager = new ArenaManager;
+        $this->arenaSetupManager = new ArenaSetupManager;
+        $this->levelSetupManager = new LevelSetupManager;
+    }
+
+    public function onDisable() {
+        $this->getArenaManager()->saveArenas();
+        $this->getLevelManager()->saveLevels();
+    }
+
+    private function registerCommands() {
+        $this->commands["bedwars"] = new EggWarsCommand;
+        $this->commands["vote"] = new VoteCommand;
+        $this->commands["team"] = new TeamCommand;
+        foreach ($this->commands as $command) {
+            $this->getServer()->getCommandMap()->register("bedwars", $command);
+        }
+    }
+
+    /**
+     * @return ArenaSetupManager $arenaSetupManager
+     */
+    public function getSetupManager(): ArenaSetupManager {
+        return $this->arenaSetupManager;
+    }
+
+    /**
+     * @return LevelSetupManager $levelSetupManager
+     */
+    public function getLevelSetupManager(): LevelSetupManager {
+        return $this->levelSetupManager;
+    }
+
+    /**
+     * @return ArenaManager $arenaManager
+     */
+    public function getArenaManager(): ArenaManager {
+        return $this->arenaManager;
+    }
+
+    /**
+     * @return LevelManager $levelManager
+     */
+    public function getLevelManager(): LevelManager {
+        return $this->levelManager;
+    }
+
+    /**
+     * @return ConfigManager
+     */
+    public function getConfigManager(): ConfigManager {
+        return $this->configManager;
+    }
+
+    /**
+     * @return string $prefix
+     */
+    public static function getPrefix(): string {
+        return ConfigManager::getPrefix()." ";
+    }
+
+    /**
+     * @return EggWars $plugin
+     */
+    public static function getInstance(): EggWars {
+        return self::$instance;
+    }
+}
